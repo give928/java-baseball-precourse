c13421280cb69be218b6e44e83c8b5fa849547bb
joohokim <give928@gmail.com>
2022-09-29 16:44:43 +0900

    feat: 볼 숫자 원시 값 포장 기능 구현
    1 ~ 9 사이의 숫자 원시 값 포장
    그 외의 숫자는 IllegalArgumentException 발생

---
+++ src/main/java/baseball/domain/Number.java
@@ -0,0 +1,43 @@
+package baseball.domain;
+
+import java.util.Objects;
+
+public class Number {
+    private static final int MIN = 1;
+    private static final int MAX = 9;
+    static final String INVALID_VALUE_MESSAGE = "볼 숫자는 1 ~ 9 사이로 입력해주세요.";
+
+    private final int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public static Number from(int value) {
+        validate(value);
+        return new Number(value);
+    }
+
+    private static void validate(int value) {
+        if (value < MIN || MAX < value) {
+            throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
