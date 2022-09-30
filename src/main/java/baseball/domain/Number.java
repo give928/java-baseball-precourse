13ea7af201fee9a7ad81902666a0802eb8e27990
joohokim <give928@gmail.com>
2022-09-30 19:28:20 +0900

    refactor: 볼 숫자 인스턴스 캐싱

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Number {
@@ -7,15 +9,23 @@ public class Number {
     static final int MAX = 9;
     static final String INVALID_VALUE_MESSAGE = "볼 숫자는 1 ~ 9 사이로 입력해주세요.";
 
+    private static final Map<Integer, Number> NUMBER_MAP;
+    static {
+        NUMBER_MAP = new HashMap<>();
+        for (int i = MIN; i <= MAX; i++) {
+            NUMBER_MAP.put(i, new Number(i));
+        }
+    }
+
     private final int value;
 
-    public Number(int value) {
+    private Number(int value) {
         this.value = value;
     }
 
     public static Number from(int value) {
         validate(value);
-        return new Number(value);
+        return NUMBER_MAP.get(value);
     }
 
     private static void validate(int value) {
