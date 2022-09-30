c96792f3729ea47668e5235f802568fc5f240f8f
joohokim <give928@gmail.com>
2022-09-30 17:01:02 +0900

    feat: input view 기능 구현
    메시지를 출력하고 입력 받은 문자열을 반환하는 기능 구현
    - 숫자를 입력해주세요 :
    - 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.

---
+++ src/main/java/baseball/ui/InputView.java
@@ -0,0 +1,31 @@
+package baseball.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public final class InputView {
+    private static final String NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    static final String RESTART = "1";
+    static final String EXIT = "2";
+    private static final String RESTART_MESSAGE = String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.", RESTART, EXIT);
+
+    private InputView() {
+    }
+
+    public static String inputNumbers() {
+        write(NUMBERS_MESSAGE);
+        return read();
+    }
+
+    public static String inputRestart() {
+        write(RESTART_MESSAGE);
+        return read();
+    }
+
+    private static void write(String message) {
+        System.out.println(message);
+    }
+
+    public static String read() {
+        return Console.readLine();
+    }
+}
