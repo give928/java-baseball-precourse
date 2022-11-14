300e0e107640d0f347ded1d25580c39a5896a9c9
joohokim <give928@gmail.com>
2022-11-14 13:57:39 +0900

    refactor: 인스턴스 캐싱 LazyHolder

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -1,21 +1,11 @@
 package baseball.domain;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class Number {
     static final int MIN = 1;
     static final int MAX = 9;
     static final String INVALID_VALUE_MESSAGE = String.format("볼 숫자는 %d ~ %d 사이로 입력해주세요.", MIN, MAX);
-
-    private static final Map<Integer, Number> NUMBER_MAP;
-    static {
-        NUMBER_MAP = new HashMap<>();
-        for (int i = MIN; i <= MAX; i++) {
-            NUMBER_MAP.put(i, new Number(i));
-        }
-    }
 
     private final int value;
 
@@ -25,12 +15,24 @@ public class Number {
 
     public static Number from(int value) {
         validate(value);
-        return NUMBER_MAP.get(value);
+        return NumberCache.cache[value + (-MIN)];
     }
 
     private static void validate(int value) {
         if (value < MIN || MAX < value) {
             throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
+        }
+    }
+
+    private static class NumberCache {
+        static final Number[] cache;
+
+        static {
+            cache = new Number[(MAX - MIN) + 1];
+            int j = MIN;
+            for(int i = 0; i < cache.length; i++) {
+                cache[i] = new Number(j++);
+            }
         }
     }
 
--- src/main/java/baseball/domain/Position.java
+++ src/main/java/baseball/domain/Position.java
@@ -1,21 +1,11 @@
 package baseball.domain;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class Position {
     private static final int MIN = 1;
     static final int MAX = 3;
     static final String INVALID_VALUE_MESSAGE = String.format("볼 위치는 %d ~ %d 사이의 숫자로 입력해주세요.", MIN, MAX);
-
-    private static final Map<Integer, Position> POSITION_MAP;
-    static {
-        POSITION_MAP = new HashMap<>();
-        for (int i = MIN; i <= MAX; i++) {
-            POSITION_MAP.put(i, new Position(i));
-        }
-    }
 
     private final int value;
 
@@ -25,12 +15,24 @@ public class Position {
 
     public static Position from(int value) {
         validate(value);
-        return POSITION_MAP.get(value);
+        return PositionCache.cache[value + (-MIN)];
     }
 
     private static void validate(int value) {
         if (value < MIN || MAX < value) {
             throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
+        }
+    }
+
+    private static class PositionCache {
+        static final Position[] cache;
+
+        static {
+            cache = new Position[(MAX - MIN) + 1];
+            int j = MIN;
+            for(int i = 0; i < cache.length; i++) {
+                cache[i] = new Position(j++);
+            }
         }
     }
 
